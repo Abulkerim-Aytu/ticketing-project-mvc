f6072862f88891d35a3aefc7c060e2635d8940fa
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-22 14:32:08 +0300

    part-1 updating 6

---
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl extends AbstractMapService<UserDTO,String> implements UserService {
+    @Override
+    public UserDTO save(UserDTO user) {
+        return super.save(user.getUserName(),user);
+    }
+
+    @Override
+    public UserDTO findById(String username) {
+        return super.findById(username);
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String username) {
+        super.deleteById(username);
+    }
+}
