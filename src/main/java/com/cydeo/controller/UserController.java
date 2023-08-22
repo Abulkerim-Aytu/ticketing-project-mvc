0058508b93062ec8394c761b987b7bfbbe75fa75
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-22 12:13:05 +0300

    part-1 updating 4

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +12,9 @@ public class UserController {
 
 
     @GetMapping("/create")
-    public String createUser(){
+    public String createUser(Model model){
+
+        model.addAttribute("user",new UserDTO());
         return "/user/create";
     }
 }
