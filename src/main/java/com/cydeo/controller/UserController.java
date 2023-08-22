410fb0b8e24b7e3f0a0f485f0130d5c2756cf91e
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-22 20:32:47 +0300

    part-2 updating 4

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,4 +28,16 @@ public class UserController {
 
         return "/user/create";
     }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user") UserDTO user){
+
+        userService.save(user);
+
+        return "redirect:/user/create";
+
+    }
+
+
+
 }
