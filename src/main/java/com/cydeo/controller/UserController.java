8a4a9cfd4465e2efaa05a15b4398539595fbc9db
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-22 13:58:00 +0300

    part-1 updating 5

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -15,6 +15,7 @@ public class UserController {
     public String createUser(Model model){
 
         model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",...;
         return "/user/create";
     }
 }
