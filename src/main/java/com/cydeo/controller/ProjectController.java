0dc208a5243e7b51cac60d4affd237a246caa756
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-23 10:57:42 +0300

    part-3 updating 5

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,18 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping("/project")
+public class ProjectController {
+
+    @GetMapping("/create")
+    public String createProject(){
+
+
+        return "/project/create";
+    }
+}
