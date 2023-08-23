42abc6569a5379ecde5e7574b11ea7b1ab87602f
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-23 12:02:40 +0300

    part-3 updating 8

--- src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -1,14 +1,17 @@
 package com.cydeo.boostrap;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
-import com.cydeo.service.impl.RoleServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 
 @Component
@@ -17,10 +20,12 @@ public class DataGenerator implements CommandLineRunner {
 
     private final RoleService roleService;
     private final UserService userService;
+    private final ProjectService projectService;
 
-    public DataGenerator(RoleService roleService, UserService userService) {
+    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService) {
         this.roleService = roleService;
         this.userService = userService;
+        this.projectService = projectService;
     }
 
     @Override
@@ -60,5 +65,15 @@ public class DataGenerator implements CommandLineRunner {
         userService.save(user6);
         userService.save(user7);
         userService.save(user8);
+
+        ProjectDTO project1 = new ProjectDTO("Spring MVC", "PR001", user1, LocalDate.now(), LocalDate.now().plusDays(25), "Creating Controllers", Status.OPEN);
+        ProjectDTO project2 = new ProjectDTO("Spring ORM", "PR002", user2, LocalDate.now(), LocalDate.now().plusDays(10), "Creating Database", Status.IN_PROGRESS);
+        ProjectDTO project3 = new ProjectDTO("Spring Container", "PR003", user1, LocalDate.now(), LocalDate.now().plusDays(32), "Creating Container", Status.IN_PROGRESS);
+
+        projectService.save(project1);
+        projectService.save(project2);
+        projectService.save(project3);
+
+
     }
 }
--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,17 +1,30 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@AllArgsConstructor
 @RequestMapping("/project")
 public class ProjectController {
 
-    @GetMapping("/create")
-    public String createProject(){
+    private final UserService userService;
+    private final ProjectService projectService;
 
+    @GetMapping("/create")
+    public String createProject(Model model) {
+
+        model.addAttribute("project", new ProjectDTO());
+
+        model.addAttribute("managers", userService.findAll());
+
+        model.addAttribute("projects", projectService.findAll());
 
         return "/project/create";
     }
